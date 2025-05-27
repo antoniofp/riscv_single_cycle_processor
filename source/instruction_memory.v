cd26d95e103c15aa4de609959022182cc5903b8e
antoniofp <j.antoniofdez89@gmail.com>
2025-05-26 23:18:33 -0600

    fix: changed instruction_memory to change memory index every 4 bytes (this memory is not designed to be byte adressable)

--- source/instruction_memory.v
+++ source/instruction_memory.v
@@ -12,7 +12,10 @@ module instruction_memory(
 	end
 	
 
-	assign instr = rom[addr]; 
+	assign instr = (addr[31:2] < 1024) ? rom[addr[31:2]] : 32'h00000000; // regresar 0 si la dirección está fuera de rango
+	// addr[31:2] se usa para acceder a la memoria de instrucciones, ya que cada instrucción es de 4 bytes (32 bits)
+	// it is the same as addr >> 2, we divide by 4 to get the index in the rom array
+
 	
 
 
